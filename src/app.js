bb5f08e3ec42130c1d776c6a033b194579b8d30d
DOTFOODS\connor.stevens <connor.stevens@dotfoods.com>
2019-04-19 15:11:47 -0500

    Module 3

--- src/app.js
+++ src/app.js
@@ -8,12 +8,13 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({extended: true}));
 
-const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), {encoding: 'UTF8'});
+const accountData = fs.readFileSync(path.join(__dirname, 'json', 'accounts.json'), 'utf8');
 
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), {encoding: 'UTF8'});
+const userData = fs.readFileSync(path.join(__dirname, 'json', 'users.json'), 'utf8');
 
 const users = JSON.parse(userData);
 
@@ -35,6 +36,37 @@ app.get('/credit', (req, res) => {
 
 app.get('/profile', (req, res) => {
     res.render('profile', {user: users[0]});
+});
+
+app.get('/transfer', (req, res) => {
+    res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+    const from = req.body.from;
+    const to = req.body.to;
+    const transfer = parseInt(req.body.amount);
+    accounts[from].balance -= transfer;
+    accounts[to].balance += transfer;
+
+    let accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+    res.render('transfer', {message: 'Transfer Completed'});
+});
+
+app.get('/payment', (req, res) => {
+    res.render('payment', {account: accounts.credit});
+})
+
+app.post('/payment', (req, res) => {
+    accounts.credit.balance -= parseInt(req.body.amount);
+    accounts.credit.available += parseInt(req.body.amount);
+
+    const accountsJSON = JSON.stringify(accounts);
+    fs.writeFileSync(path.join(__dirname, 'json', 'accounts.json'), accountsJSON, 'utf8');
+
+    res.render('payment', {message: 'Payment Successful', account: accounts.credit});
 });
 
 app.listen(3000, () => {
